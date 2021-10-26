939e005e113cd52d816418d895aed212e5fc01ae
Kishan <73935756+kishankishore29@users.noreply.github.com>
2021-10-27 02:26:05 +0545

    PER-16:Added id field to the migration file and the seeder (#2)

--- internal/models/person.go
+++ internal/models/person.go
@@ -1,6 +1,7 @@
 package models
 
 type Person struct {
+	Id      string `gorm:"default:uuid_generate_v4()"` //ID of the person-record
 	Name    string // Name of the person
 	Age     int32  // Age of the Person
 	Email   string // Email address of the person
